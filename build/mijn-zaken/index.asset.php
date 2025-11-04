c2ff8c32d10808bb02f658dd35cfec0f20400e99
YvetteNikolov <yvettenikolov@gmail.com>
2025-11-04 16:50:22 +0100

    feat: add denhaag-case-card element to owc-overview-zaken

--- build/mijn-zaken/index.asset.php
+++ build/mijn-zaken/index.asset.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render' ),
-	'version'      => 'f7409b1189a3f18aca64',
-);
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '7748e84f0b72a02bb8ef');
