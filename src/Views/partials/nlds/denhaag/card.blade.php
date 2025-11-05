141f8125693b3108cb551b1c7e78833ffffbcaea
YvetteNikolov <yvettenikolov@gmail.com>
2025-11-05 11:45:28 +0100

    chore: add originele aanvraag with description-list component

--- src/Views/partials/nlds/denhaag/card.blade.php
+++ src/Views/partials/nlds/denhaag/card.blade.php
@@ -1,4 +1,10 @@
-<div class="js-nlds-denhaag-case-card-component" data-appearance="{{ $appearance ?? '' }}"
-	data-title="{{ $zaak->title() ?? '' }}" data-sub-title="{{ $subTitle ?? '' }}"
-	data-context="{{ $zaak->startDate('j F Y') ?? '' }}" data-href="{{ $zaak->permalink() ?? '' }}"
-	@if (!empty($zaak->startDate())) data-datetime="{{ $zaak->startDate('Y-m-d') }}" @endif></div>
+@php
+	$zaak ??= null;
+@endphp
+
+@if ($zaak)
+	<div class="js-nlds-denhaag-case-card-component" data-appearance="{{ $appearance ?? '' }}"
+		data-title="{{ $zaak->title() ?? '' }}" data-sub-title="{{ $subTitle ?? '' }}"
+		data-context="{{ $zaak->startDate('j F Y') ?? '' }}" data-href="{{ $zaak->permalink() ?? '' }}"
+		@if (!empty($zaak->startDate())) data-datetime="{{ $zaak->startDate('Y-m-d') }}" @endif></div>
+@endif
