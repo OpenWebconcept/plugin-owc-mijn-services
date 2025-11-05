bff5e788dc2c471272ca0e32ee57017db1df4f98
YvetteNikolov <yvettenikolov@gmail.com>
2025-11-05 09:21:08 +0100

    feat: refactor to use map

--- build/mijn-zaken/index.asset.php
+++ build/mijn-zaken/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '7748e84f0b72a02bb8ef');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '53420b270b04a2716efb');
---
+++ build/zaak/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '673e517dacac2ee4df56');
