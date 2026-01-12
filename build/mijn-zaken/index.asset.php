564698d1bf8a1bd3dc88c8b21356f43d97ce7bdf
Mike van den Hoek <mike@yard.nl>
2026-01-12 16:27:57 +0100

    feat: implement kvk usage

--- build/mijn-zaken/index.asset.php
+++ build/mijn-zaken/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ab2ec9e9526dd2830637');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'c255df62cd6927a2b215');
