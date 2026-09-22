be31691d368a79624849e5871bf8462bc0bfae58
Mike van den Hoek <mike.vandenhoek@yard.nl>
2026-09-22 11:59:29 +0200

    feat: use placeholder component in owc-my-services/zaak block

--- build/zaak/index.asset.php
+++ build/zaak/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '27206ca7dc09304c66a0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '25cca51eb2e610a50d16');
